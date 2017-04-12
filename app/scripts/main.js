ef07e8fdc306412c8e3e6a7f306be8e641597d6a
wanghao8080 <117663444@qq.com>
2017-04-12 16:17:06 +0800

    搜索框：交互

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
 (function() {
   'use strict';
   // Your custom JavaScript goes here
-  
+
+  /**
+   * navbar
+   */
+  $('.ui.navbar .search-icon > i')
+    .click(function () {
+      let icon = $(this)
+      if (icon.text() === 'search') {
+        icon.text('close')
+      } else {
+        icon.text('search')
+      }
+    })
 
 })();
