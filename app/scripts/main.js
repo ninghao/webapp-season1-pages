8118c56e5f50baf35cf43aa2132baeb3dd8c49d7
wanghao8080 <117663444@qq.com>
2017-04-19 10:24:56 +0800

    改进旋转木马样式

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,8 +33,8 @@
   $('.ui.hero.carousel')
     .slick({
       // arrows: false,
-      prevArrow: '<button class="ui prev button"><i class="material-icons">chevron_left</i></button>',
-      nextArrow: '<button class="ui next button"><i class="material-icons">chevron_right</i></button>',
+      prevArrow: '<button class="ui prev bottom button"><i class="material-icons">chevron_left</i></button>',
+      nextArrow: '<button class="ui next bottom button"><i class="material-icons">chevron_right</i></button>',
 
       responsive: [
         {
